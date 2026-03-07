9ebe6da715c9f10252a857950f126ec4c3dfb6d8
Divyansh-chaudhary <chdivyansh9@gmail.com>
2026-03-07 12:03:11 +0530

    DSA: bubble sort with recursion

--- sorting/bubble-sort.js
+++ sorting/bubble-sort.js
@@ -4,29 +4,40 @@
 
 const arr = [10, 34, 9, 12, 43, 1];
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
-    if (arr[i] > arr[j]) {
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+for (let i = arr.length; i > 0; i--) {
+  for (let j = 0; j < i; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
 }
 
-console.log(arr); // worst case O(n2) , average case same
+console.log(arr, ": brute force with loop"); // worst case O(n2) , average case same
 
-for (let i = 0; i < arr.length; i++) {
-  for (let j = i + 1; j < arr.length; j++) {
+const recursionBubbleSort = (arr, n) => {
+  if (n === 1) return arr;
+  for (let i = 0; i < n - 2; i++) {
+    if (arr[i] > arr[i + 1]) {
+      [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+    }
+  }
+  return recursionBubbleSort(arr, n - 1);
+};
+
+console.log(
+  recursionBubbleSort(arr, arr.length),
+  ": brute force with recursion",
+); // worst case O(n2) , average case same
+
+for (let i = arr.length; i > 0; i--) {
+  for (let j = 0; j < i; j++) {
     let didSwap = false;
-    if (arr[i] > arr[j]) {
-      const temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
       didSwap = true;
     }
     if (!didSwap) break;
   }
 }
 
-console.log(arr); // worst case O(n2) , average case O(n)
+console.log(arr, ": brute force and O(n) for already sorted"); // worst case O(n2) , average case O(n)
