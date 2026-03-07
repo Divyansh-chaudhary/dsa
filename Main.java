02b643853cf3c28e3a3f614856d05e65e7b3d838
Divyansh-chaudhary <chdivyansh9@gmail.com>
2026-03-07 09:36:44 +0530

    java: method overloading

--- Main.java
+++ Main.java
@@ -2,11 +2,16 @@ public class Main {
     public static void main(String[] s) {
         Calculator calculator = new Calculator();
         System.out.println(calculator.sum(3, 4));
+        System.out.println(calculator.sum(3, 4, 5));
     }
 }
 
 class Calculator {
     public int sum(int a, int b) {
         return a + b;
+    }
+
+    public int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
