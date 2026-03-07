3ca94b3bccc597374e7ea942fa42ddf553d82217
Divyansh-chaudhary <chdivyansh9@gmail.com>
2026-03-07 09:53:48 +0530

    java: added array

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] s) {
+        int arr[] = { 1, 2, 3, 4, 5 };
+        int dynamicArr[] = new int[4];
+        dynamicArr[1] = 5;
         Calculator calculator = new Calculator();
         System.out.println(calculator.sum(3, 4));
         System.out.println(calculator.sum(3, 4, 5));
+        System.out.println(arr[0]);
+        System.out.println(dynamicArr[0]);
+        System.out.println(dynamicArr[1]);
     }
 }
 
