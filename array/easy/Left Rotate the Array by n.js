45bdd4175d5821cd92c45892c50c18d7132cd683
Divyansh-chaudhary <chdivyansh9@gmail.com>
2026-03-10 21:56:16 +0530

    dsa

--- array/easy/Left Rotate the Array by n.js
+++ array/easy/Left Rotate the Array by n.js
@@ -1,5 +1,4 @@
 // brute force space O(n) time O(n)
-
 const rotate = (arr, k) => {
   const firstArr = [];
   const secArr = [];
@@ -9,6 +8,33 @@ const rotate = (arr, k) => {
   }
   return [...firstArr, ...secArr];
 };
-const arr = [1, 2, 3, 4, 5];
+
+const arr = [1, 2, 3, 4, 5, 6];
 const result = rotate(arr, 2);
 console.log(result);
+
+const isEven = (num) => num % 2 === 0;
+/**
+ * reverse the whole array
+ * than reverse the k element
+ * reverse the n -k elements
+ */
+const rotate2 = (arr, k) => {
+  for (let i = 0; i < arr.length / 2 - (isEven(arr.length) ? 0 : 1); i++)
+    [arr[i], arr[arr.length - 1 - i]] = [arr[arr.length - 1 - i], arr[i]];
+
+  for (let i = 0; i < k / 2 - (isEven(k) ? 0 : 1); i++)
+    [arr[i], arr[k - 1 - i]] = [arr[k - 1 - i], arr[i]];
+
+  for (let i = k, j = arr.length - 1; i < j; i++, j--) {
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+};
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr3 = [1, 2, 3, 4, 5, 6, 7];
+const k = 3;
+rotate2(arr1, k);
+rotate2(arr3, k);
+console.log(JSON.stringify(arr1));
+console.log(JSON.stringify(arr3));
